adedf33bea522ef198a7593fd5d0324849a594a4
Natnael Lijato <natnael.liza@gmail.com>
2016-10-04 16:26:45 +0200

    This is testing commit gitHub

---
+++ src/com/company/Customer.java
@@ -0,0 +1,41 @@
+package com.company;
+
+/**
+ * Created by natna on 10/4/2016.
+ */
+public class Customer {
+
+    private String name;
+    private String address;
+    private int age;
+
+    public Customer(String name, String address, int age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
