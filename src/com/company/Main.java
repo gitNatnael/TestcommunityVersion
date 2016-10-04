bcc7e1fd235a9c290d2e9ae8db63130d5dcd5d96
Natnael Lijato <natnael.liza@gmail.com>
2016-10-04 16:10:41 +0200

    This is testing commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] ara={1,2,3,4};
+        for (int i: ara)
+            System.out.println(i);
+        System.out.println("I AM EHRE");
+        // write your code here
+    }
+}
